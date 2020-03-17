bd7fa496db6c02c2b9dcd3a2ba9f7ec697c95414
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-03-17 14:36:43 +0000

    added raising temp

--- jasmine-standalone-3/spec/thermonstatspec.js
+++ jasmine-standalone-3/spec/thermonstatspec.js
@@ -1,11 +1,19 @@
+'use strict';
+
 describe('Thermostat', function() {
   var thermostat;
 
   beforeEach(function() {
     thermostat = new Thermostat();
   });
+
   it('starts at 20', function() {
     expect(thermostat.temperature).toEqual(20);
+  });
+
+  it('increases temperature', function() {
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
   });
 
 
