ba928bfc16f9d764cc3cea83f388b29d6ae7d160
George Swarbrick <glswarbrick@hotmail.co.uk>
2020-03-19 14:12:59 +0000

    added API for london temperature

--- interface.js
+++ interface.js
@@ -2,35 +2,51 @@ $(document).ready(function() {
     var thermostat = new Thermostat();
     updateTemperature();
 
+    displayWeather('London');
+    $('#select-city').submit(function(event) {
+      event.preventDefault();
+      var city = $('#current-city').val();
+      displayWeather(city);
+     });
+
   $('#up').on('click', function() { 
     thermostat.up(1);
     updateTemperature();
-  })
+  });
 
   $('#down').on('click', function() {
     thermostat.down(1);
     updateTemperature();
-  })
+  });
 
   $('#reset').on('click', function() {
     thermostat.reset();
     updateTemperature();
-  })
+  });
 
   $('#psm-on').on('click', function() {
     thermostat.switchPowerSavingModeOn();
     $('#power-saving-status').text("on");
     updateTemperature();
-  })
+  });
 
   $('#psm-off').on('click', function() {
     thermostat.switchPowerSavingModeOff();
     $('#power-saving-status').text("off");
     updateTemperature();
-  })
+  });
 
   function updateTemperature() {
     $('#temperature').text(thermostat.temperature);
     $('#temperature').attr('class', thermostat.usage());
+  }
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#current-temperature').text(data.main.temp);
+    });
   }
 })
